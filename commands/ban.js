4bbad393c9a0d3a371c147e59c4aacea80c82d04
Dayan <dayandivinate@gmail.com>
2022-05-09 05:18:41 +1200

    Error handling + unban command

--- commands/ban.js
+++ commands/ban.js
@@ -2,7 +2,7 @@ const { Permissions, Constants } = require("discord.js");
 
 module.exports = {
 	data: {
-		name: "sban",
+		name: "sban6",
 		description: "Ban user from the server",
 		options: [
 			{
@@ -25,6 +25,8 @@ module.exports = {
 		const banReason = interaction.options.getString("reason", true);
 		const commandUser = interaction.member.user;
 		
+		const banNotes = [];
+		
 		if(!interaction.memberPermissions.has(Permissions.FLAGS.BAN_MEMBERS)) return;	//check commandUser permissions
 		
 		if(!interaction.guild.me.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {		//check bot permissions
@@ -32,19 +34,29 @@ module.exports = {
 			return;
 		}
 		
-		if(interaction.guild.members.resolve(userToBan).permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {	//check userToBan permissions
+		if(!interaction.guild.members.resolve(userToBan)) {	//check if userToBan is a member of the guild
+			banNotes.push("User not in guild prior to ban");
+		} else if(interaction.guild.members.resolve(userToBan).permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {	//check userToBan permissions
 			await interaction.reply("This user cannot be banned.");
 			return;
 		}
 		
-		await userToBan.send(`You have been banned from ${interaction.guild.name}. Reason: ${banReason}`);
-		
-		const banString = `
+		let banString = `
 			**Banned user:** <@${userToBan.id}> (${userToBan.id})
 			**Reason:** ${banReason}
 			**Duration:** Permanent
 			**Staff member:** ${commandUser.username}
 		`.replaceAll("\t","");
+		
+		try {
+			await userToBan.send(`You have been banned from ${interaction.guild.name}. Reason: ${banReason}`);
+		} catch(err) {
+			banNotes.push(err.message);
+		}
+		
+		for(let i = 0; i < banNotes.length; i++) {
+			banString += `\n**Note**: ${banNotes[i]}`;
+		}
 		
 		await interaction.guild.bans.create(userToBan,
 			{
@@ -53,6 +65,8 @@ module.exports = {
 			.then(async (banInfo) => {
 				await interaction.reply(banString);
 			})
-			.catch(console.error);
+			.catch(async (err) => {
+				await interaction.reply(`Error: ${err.message}`);
+			});
 	}
 };
---
+++ commands/unban.js
@@ -0,0 +1,36 @@
+const { Permissions, Constants } = require("discord.js");
+
+module.exports = {
+	data: {
+		name: "sunban7",
+		description: "Unban user from the server",
+		options: [
+			{
+				name: "username",
+				type: Constants.ApplicationCommandOptionTypes.USER,
+				description: "The user to unban",
+				required: true
+			}
+		]
+	},
+	
+	async execute(interaction) {
+		const userToUnban = interaction.options.getUser("username", true);
+		const commandUser = interaction.member.user;
+		
+		if(!interaction.memberPermissions.has(Permissions.FLAGS.BAN_MEMBERS)) return;	//check commandUser permissions
+		
+		if(!interaction.guild.me.permissions.has(Permissions.FLAGS.BAN_MEMBERS)) {		//check bot permissions
+			await interaction.reply(`${interaction.guild.me.user.username} does not have permissions to unban in this server.`);
+			return;
+		}
+		
+		await interaction.guild.bans.remove(userToUnban)
+		.then(async () => {
+			await interaction.reply(`Unbanned user: <@${userToUnban.id}> (${userToUnban.id})`);
+		})
+		.catch(async (err) => {
+			await interaction.reply(`Error: ${err.message}`);
+		});
+	}
+};
