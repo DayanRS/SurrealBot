a8e71d93cd94683e2a70bb44f6db89c5d88350f1
Samuel Söderberg <samuelsoderberg99@gmail.com>
2022-06-07 14:27:56 +0200

    avatar command added option

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,4 +1,4 @@
-const { Constants} = require("discord.js");
+const { Constants } = require("discord.js");
 
 module.exports = {
 	data: {
@@ -9,20 +9,41 @@ module.exports = {
 				name: "user",
 				type: Constants.ApplicationCommandOptionTypes.USER,
 				description: "The name of the user",
+			},
+            {
+				name: "default",
+				type: Constants.ApplicationCommandOptionTypes.BOOLEAN,
+				description: "Display the user avatar instead of the server specific avatar?",
 			}
 		]
 	},
 	async execute(interaction) {
 		let targetUser = interaction.options.getUser("user");
+        const defaultAvatar = interaction.options.getBoolean("default");
 		const commandUser = interaction.member;	//as GuildMember
 		
         if(!targetUser) {
             targetUser = commandUser;
+        } else {
+            try {
+				targetUser = await interaction.guild.members.fetch(targetUser);
+			} catch(err) {
+				await interaction.reply({
+					content: `<@${targetUser.id}> (${targetUser.id}) is not a member of this server.`,
+					ephemeral: true
+				});
+				return;
+			}
         }
 
-
         await interaction.reply({
-            content: `${targetUser.displayAvatarURL({size: 4096, dynamic: true})}?size=512`
+            content: `${
+                defaultAvatar
+                ?
+                targetUser.user.displayAvatarURL({size: 4096, dynamic: true})
+                :
+                targetUser.displayAvatarURL({size: 4096, dynamic: true})
+            }`
         });
 	}
 };
