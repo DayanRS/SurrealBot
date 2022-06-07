bd90f4012cccc5091c1fcfcced8b5855cff90d52
Samuel Söderberg <samuelsoderberg99@gmail.com>
2022-06-07 23:00:20 +0200

    hug command

---
+++ commands/hug.js
@@ -0,0 +1,33 @@
+const { Constants } = require("discord.js");
+
+module.exports = {
+	data: {
+		name: "hug",
+		description: "Hug someone!",
+		options: [
+			{
+				name: "user",
+				type: Constants.ApplicationCommandOptionTypes.USER,
+				description: "The user who you want to hug",
+			}
+		]
+	},
+	async execute(interaction) {
+		let hugTarget = interaction.options.getUser("user");
+		const commandUser = interaction.member;	//as GuildMember
+		
+
+        if(!hugTarget || hugTarget.username == commandUser.user.username) {
+            await interaction.reply({
+				content: `${commandUser} hugged themselves lol`
+			});
+			return;
+        }
+
+
+        await interaction.reply({
+            content: `<@${hugTarget.id}> has been hugged by <@${commandUser.user.id}> ❤️`
+        });
+
+	},
+};
