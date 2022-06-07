54b56e361bf3cb39fee377f1d88922e5c42155aa
Samuel Söderberg <samuelsoderberg99@gmail.com>
2022-06-07 23:00:14 +0200

    iq command

---
+++ commands/iq.js
@@ -0,0 +1,39 @@
+const { Constants } = require("discord.js");
+
+module.exports = {
+	data: {
+		name: "iq",
+		description: "Check the IQ of a user",
+		options: [
+			{
+				name: "user",
+				type: Constants.ApplicationCommandOptionTypes.USER,
+				description: "The user whose IQ you want to know",
+			}
+		]
+	},
+	async execute(interaction) {
+		let iqUser = interaction.options.getUser("user");
+		const commandUser = interaction.member;	//as GuildMember
+		
+
+        if(!iqUser || iqUser.username == commandUser.user.username) {
+            await interaction.reply({
+				content: `${commandUser} has an IQ of 5.`
+			});
+			return;
+        }
+
+        let iq = this.getIQ(iqUser.username);
+
+        await interaction.reply({
+            content: `<@${iqUser.id}> has an IQ of ${iq}.`
+        });
+
+	},
+    getIQ(seed) {
+        let temp = seed.toLocaleLowerCase().split("").reduce((acc, letter) => acc + letter.charCodeAt(0) * 3, 0)
+        let iq = temp % 180;
+        return iq;
+    }
+};
