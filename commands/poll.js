f48c374cd46675f599d756fbe7a8b1e63d0ce42d
Samuel Söderberg <samuelsoderberg99@gmail.com>
2022-06-07 23:00:02 +0200

    poll command

---
+++ commands/poll.js
@@ -0,0 +1,32 @@
+const { Constants, MessageEmbed } = require("discord.js");
+
+module.exports = {
+	data: {
+		name: "poll",
+		description: "Create a poll with custom text",
+		options: [
+			{
+				name: "message",
+				type: Constants.ApplicationCommandOptionTypes.STRING,
+				description: "What are you polling?",
+                required: true
+			}
+		]
+	},
+	async execute(interaction) {
+		let pollMsg = interaction.options.getString("message");
+		const commandUser = interaction.member;	//as GuildMember
+		
+
+		const embedMessage = new MessageEmbed()
+		.setColor("#ff9b00")
+        .setTitle(pollMsg)
+		.setDescription("Polled by " + commandUser.displayName);
+		
+        const pollInteraction = await interaction.reply({ embeds: [embedMessage], fetchReply: true });
+
+
+        await pollInteraction.react("✅");
+        await pollInteraction.react("⛔");
+	}
+};
