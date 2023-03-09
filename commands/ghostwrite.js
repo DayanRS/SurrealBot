d7fce51195c08474ada9902cc6df72aefe7fd539
Samuel Söderberg <samuelsoderberg99@gmail.com>
2023-03-09 14:58:02 +0100

    ghostwrite command

---
+++ commands/ghostwrite.js
@@ -0,0 +1,34 @@
+const { Constants, PermissionFlagsBits } = require("discord.js");
+
+module.exports = {
+	data: {
+		name: "ghostwrite",
+		description: "Become an impostor sus",
+		defaultMemberPermissions: PermissionFlagsBits.KICK_MEMBERS,
+		options: [
+			{
+				name: "channel",
+				type: Constants.ApplicationCommandOptionTypes.CHANNEL,
+				description: "The channel in which to send the message",
+			},
+			{
+				name: "message",
+				type: Constants.ApplicationCommandOptionTypes.STRING,
+				description: "The message to send",
+			}
+		]
+	},
+	
+	async execute(interaction) {
+		await interaction.deferReply();
+		interaction.isDeferred = true;
+		
+		const channel = interaction.options.getChannel("channel");
+		const message = interaction.options.getChannel("message");
+		const commandUser = interaction.member;	//as GuildMember
+
+		message = message.replace(/(@everyone|@here)/gmi, "")
+
+		await welcomeChannel.send({ content: message });
+	}
+};
