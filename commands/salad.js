705e6595d3237d66b9b6a4497f5f7caab0cc0d34
Samuel Söderberg <samuelsoderberg99@gmail.com>
2023-04-07 13:42:48 +0200

    salad mode disabled

--- commands/salad.js
+++ commands/salad.js
@@ -7,31 +7,37 @@ module.exports = {
 	async execute(interaction) {
 		await interaction.deferReply();
 		interaction.isDeferred = true;
+
+		await interaction.editReply({
+			content: `This is illegal. Check behind you.`,
+			ephemeral: true
+		});
+		return;
 		
-		const saladRole = ((await interaction.guild.roles.fetch()).filter((role) => role.name === "Salad")).at(0);
+		// const saladRole = ((await interaction.guild.roles.fetch()).filter((role) => role.name === "Salad")).at(0);
 		
-		if(!saladRole) {
-			await interaction.editReply({
-				content: `"Salad" role does not exist in this server.`,
-				ephemeral: true
-			});
-			return;
-		}
+		// if(!saladRole) {
+		// 	await interaction.editReply({
+		// 		content: `"Salad" role does not exist in this server.`,
+		// 		ephemeral: true
+		// 	});
+		// 	return;
+		// }
 		
-		if(interaction.member.roles.cache.some(role => role.id === saladRole.id)) {
-			await interaction.member.roles.remove(saladRole);
-			await interaction.editReply({
-				content: `Salad mode disabled`,
-				ephemeral: true
-			});
-			return;
-		} else {
-			await interaction.member.roles.add(saladRole);
-			await interaction.editReply({
-				content: `:salad: ${interaction.member.user.username} enabled salad mode! :salad:`,
-				ephemeral: true
-			});
-			return;
-		}
+		// if(interaction.member.roles.cache.some(role => role.id === saladRole.id)) {
+		// 	await interaction.member.roles.remove(saladRole);
+		// 	await interaction.editReply({
+		// 		content: `Salad mode disabled`,
+		// 		ephemeral: true
+		// 	});
+		// 	return;
+		// } else {
+		// 	await interaction.member.roles.add(saladRole);
+		// 	await interaction.editReply({
+		// 		content: `:salad: ${interaction.member.user.username} enabled salad mode! :salad:`,
+		// 		ephemeral: true
+		// 	});
+		// 	return;
+		// }
 	}
 };
